c2e92b292a876d82f3ab67b6f31a8d8efa240745
kparisi <kristenparisi3@gmail.com>
2024-08-05 10:43:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -55,13 +55,13 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit
-  if (remaingCalories < 0){
-    surplusOrDeficit = "Surplus"
-  } else{
-    surplusOrDeficit = "Deficit"
-  }
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
 
+  `;
 }
 
 function getCaloriesFromInputs(list) {
